8ea0fd55658470a9e8798173302c5930ecc78de9
mokupsas <mockus.ri@gmail.com>
2023-02-24 14:59:21 +0200

    added taskType and statusType enums

---
+++ taskType.lua
@@ -0,0 +1,4 @@
+TASK_TYPE_HOST     = "host" --search for host 
+TASK_TYPE_DOWNLOAD = "download" --download speed
+TASK_TYPE_UPLOAD   = "upload"  --upload speed
+TASK_TYPE_AUTO     = "auto"  --upload and download speed
